b80c1cfe6694f7df5d29971b9f69d874fd60cfd6
Mahmovdsayed <mahmoudsayed3576@gmail.com>
2026-02-02 02:42:28 +0200

    Refactor code structure for improved readability and maintainability

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
 
 const page = () => {
   return <>
-    <div className="flex flex-col gap-2 items-center justify-center min-h-dvh">
-      <h1 className="text-4xl font-bold">Hirely</h1>
-      <p className="text-lg">AI-Powered Freelance and Portfolio Platform</p>
-    </div>
+    <h1>Welcome to Hirely</h1>
   </>;
 };
 
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  cacheComponents: false,
+  cacheMaxMemorySize: 0,
 };
 
 export default nextConfig;
